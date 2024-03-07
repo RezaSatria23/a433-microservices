6086cabdcdc80b957a873e7beab9cfa23e86708e
Reza Satria Tama <rezasatriatama69@gmail.com>
2024-03-07 10:55:45 +0700

    new

--- build_push_image.sh
+++ build_push_image.sh
@@ -6,15 +6,15 @@ image_version="v1"
 github_account="rezasatria23"
 
 # membuat image di Docker
-echo -e "Building a new Docker image:"
+echo -e "Membuat sebuah image docker baru"
 docker build . -t $image_name:$image_version
 
 # melihat daftar image di lokal
-echo -e "\nViewing the list of Docker images:"
+echo -e "\nMelihat List Docker images:"
 docker images
 
 # mengubah nama image agar sesuai dengan format GitHub Container Registry
-echo -e "\nGiving a new tag name to the Docker image to match the format of GitHub Container Registry:"
+echo -e "\nMemberikan Nama Tag baru pada image Docker agar sesuai dengan format  GitHub Container Registry:"
 docker tag $image_name:$image_version ghcr.io/$github_account/$image_name:$image_version
 docker images
 
@@ -26,6 +26,7 @@ docker login ghcr.io -u $github_account
 echo -e "\nPublishing image to GitHub Container Registry:"
 docker push ghcr.io/$github_account/$image_name:$image_version
 
+# selesai proses push ke GitHub Container Registry
 echo -e "\nDone."
 
 # create BY Reza Satria Tama
